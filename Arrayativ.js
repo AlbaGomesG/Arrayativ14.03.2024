fb4985460f95f711e3f618e54cdc33e497433867
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-15 14:28:45 -0300

    Coloquei o pop

--- Arrayativ.js
+++ Arrayativ.js
@@ -6,3 +6,6 @@ let numbers = [1, 2, 3, 4];
 numbers.shift(); // Vai remover o primeiro elemento do Array
 console.log(numbers) //[2, 3, 4];
 
+let número = [1, 2, 3, 4];
+número.pop(4); // Vai removero último elemento do Array
+console.log(número) //[1, 2, 3];
