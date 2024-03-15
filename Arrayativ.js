c4715e1a323c7969b7ca88bf90b1d1e70731c3ce
AlbaGomesG <giovannaalba.tds1.senai@gmail.com>
2024-03-15 14:23:53 -0300

    Coloquei o shift

--- Arrayativ.js
+++ Arrayativ.js
@@ -1,3 +1,8 @@
 let numeros = [1, 2, 3, 4, 5];
 numeros.push(6, 7); // Vai adicionar os números 6 e 7 no final do Array
 console.log(numeros) //[1, 2, 3, 4, 5, 6, 7];
+
+let numbers = [1, 2, 3, 4];
+numbers.shift(); // Vai remover o primeiro elemento do Array
+console.log(numbers) //[2, 3, 4];
+
